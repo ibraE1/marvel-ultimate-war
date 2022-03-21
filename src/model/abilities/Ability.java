bc8175fe4010f901ce3345276f6946f1e57c457a
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-03-21 19:54:04 +0200

    Added Effect class and started Ability class

---
+++ src/model/abilities/Ability.java
@@ -0,0 +1,54 @@
+package model.abilities;
+
+public class Ability {
+    private String name;
+    private int manaCost;
+    private int baseCooldown;
+    private int currentCooldown;
+    private int castRange;
+    private int requiredActionPoints;
+    private AreaOfEffect castArea;
+
+    public Ability(String name,int cost, int baseCoolDown, int castRange, AreaOfEffect area , int required){
+        super();
+        this.name = name;
+        this.manaCost = cost;
+        this.baseCooldown = baseCoolDown;
+        this.castRange = castRange;
+        this.castArea = area;
+        this.requiredActionPoints = required;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getManaCost() {
+        return manaCost;
+    }
+
+    public int getBaseCooldown() {
+        return baseCooldown;
+    }
+
+    public int getCastRange() {
+        return castRange;
+    }
+
+    public AreaOfEffect getCastArea() {
+        return castArea;
+    }
+
+    public int getRequiredActionPoints() {
+        return requiredActionPoints;
+    }
+
+    public int getCurrentCooldown() {
+        return currentCooldown;
+    }
+
+    public void setCurrentCooldown(int currentCooldown) {
+        this.currentCooldown = currentCooldown;
+    }
+
+}
