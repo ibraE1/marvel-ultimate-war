2245c084679dbf5ddd9e01f350670d183b948e42
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-03-21 20:49:54 +0200

    Added the Cover class

---
+++ src/model/world/Cover.java
@@ -0,0 +1,25 @@
+package model.world;
+import java.awt.*;
+import java.util.*;
+public class Cover {
+
+    private int currentHP;
+    private Point location;
+
+    public Cover(int x, int y){
+        Point location = new Point(x,y);
+        int hp = (int) ((Math.random() * 1000) + 1);
+    }
+
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setCurrentHP(int currentHP) {
+        this.currentHP = currentHP;
+    }
+}
