c1dcd8bb59f24482c84f2f8f52dec44ae828aa58
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-05-13 01:18:47 +0200

    Modified Embrace, PowerUp, Root and Shock Classes

--- src/model/effects/Shock.java
+++ src/model/effects/Shock.java
@@ -1,7 +1,23 @@
 package model.effects;
 
+import model.world.Champion;
+
 public class Shock extends Effect {
     public Shock(int duration) {
         super("Shock", duration, EffectType.DEBUFF);
+    }
+
+    public void apply(Champion c) {
+        c.setSpeed((int) (c.getSpeed() * 0.9));
+        c.setAttackDamage((int) (c.getAttackDamage() * 0.9));
+        c.setCurrentActionPoints((int) (c.getCurrentActionPoints() - 1));
+        c.setMaxActionPointsPerTurn((int) (c.getMaxActionPointsPerTurn() - 1));
+    }
+
+    public void remove(Champion c) {
+        c.setSpeed((int) (c.getSpeed() / 0.9));
+        c.setAttackDamage((int) (c.getAttackDamage() / 0.9));
+        c.setCurrentActionPoints((int) (c.getCurrentActionPoints() + 1));
+        c.setMaxActionPointsPerTurn((int) (c.getMaxActionPointsPerTurn() + 1));
     }
 }
